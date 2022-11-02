0ba6520f34f05be5349929a16eaf1db6a670be0f
AndreyLytvynov <99054051+AndreyLytvynov@users.noreply.github.com>
2022-11-02 19:15:50 +0200

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ export async function getAllPages(nameImg, page = '1') {
     params: {
       key: '30999598-dc54c9ae8f4c38c0174094b65',
       image_type: 'photo',
-      per_page: 200,
+      per_page: 20,
       orientation: 'horizontal',
       safesearch: 'true',
       q: nameImg,
